559c1b043bafe1be4021c3f4e854ddd78a737d7d
Amgad Abdullah <amga200d@gmail.com>
2024-11-27 00:09:54 -0700

    a function that create an alarm schedule based on a comman line arguments passed to it

--- timer1.js
+++ timer1.js
@@ -1,9 +1,21 @@
+// scheduleAlams function
+const scheduleAlarms = function() {
+// First we need to capture the command line argument in a variable. Then converting each string value into numbers.
+const alarmTimes = process.argv.slice(2).map(Number);
 
-// First we need to capture the command line argument in a variable.
-const alarmTimes = process.argv.slice(2)
-
-// We will sort the array to get the right sequance of beebs
+// We will sort the array to get the right sequance of beebs.
 const sortedAlarm = alarmTimes.sort((a, b) => a - b);
 
-console.log(sortedAlarm);
-// process.stdout.write()
+//We will loop over the array with the times from the sorted array.
+for (let time of sortedAlarm) {
+// Conditional statment to ignore negative values and NaN
+  if (time > 0 && !isNaN(time)) {
+    setTimeout(() => {
+        process.stdout.write("\x07");
+      }, time * 1000)
+  }
+};
+} 
+
+// Call the function to execute the alarm scheduling. 
+scheduleAlarms();
